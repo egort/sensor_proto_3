3dda44d6b250e0ac4e8a715c37d5bb892b094dd9
egort <rogachevd@gmail.com>
2017-05-31 02:17:14 +0300

    + adafruit bmp280 lib

--- src/main.cpp
+++ src/main.cpp
@@ -7,57 +7,48 @@
        #####
 */
 
-#include "dht11.h"
+#include <Wire.h>
 #include <SPI.h>
-#include <plainRFM69.h>
+#include "plainRFM69.h"
+#include "Adafruit_Sensor.h"
+#include "Adafruit_BMP280.h"
 
-#define DHTPIN 3 // DHT11 sensor pin
+//#define DHTPIN 3 // DHT11 sensor pin
 #define SLAVE_SELECT_PIN 10 // SS/NSS line on SPI bus
 #define SENDER_DETECT_PIN A0 // tie this pin down on the receiver.
 #define RESET_PIN 8 // connected to the reset pin of the RFM69
 #define DIO2_PIN 2 // Pin DIO 2 on the RFM69 is attached to this digital pin which should have interrupt capability (2 or 3)
 #define INTERRUPT_NUMBER 0 // on Pro Mini INT0 at pin 2, INT1 at 3.
 
-/*
-    This is a minimal example with the interrupt to call poll().
-    It transmits a 4 byte integer every 500 ms.
-*/
+#define Addr 0x76 /// BMP280 I2C address is 0x76(108)
+#define P0 1013.25
 
-dht11 DHT11;
-plainRFM69 rfm = plainRFM69(SLAVE_SELECT_PIN);
+Adafruit_BMP280 bmp; // I2C
+bool isBMP280present = false;
+plainRFM69 rfm = plainRFM69(SLAVE_SELECT_PIN); // SPI
 
-double heatIndex(double tempC, double humidity)
+void interrupt_RFM()
 {
-  double c1 = -42.38, c2 = 2.049, c3 = 10.14, c4 = -0.2248, c5= -6.838e-3, c6=-5.482e-2, c7=1.228e-3, c8=8.528e-4, c9=-1.99e-6  ;
-  double Tf = ((tempC*9)/5)+32;
-  double R = humidity;
-  double A = (( c5 * Tf) + c2) * Tf + c1;
-  double B = ((c7 * Tf) + c4) * Tf + c3;
-  double C = ((c9 * Tf) + c8) * Tf + c6;
-  double rv = (C * R + B) * R + A;
-  return rv;
-}
-
-void interrupt_RFM(){
     rfm.poll(); // in the interrupt, call the poll function.
 }
 
 void setup() {
+  Wire.begin(); // Initialise I2C communication as MASTER
   SPI.begin();
   Serial.begin(19200);
-  Serial.println("--DHT11 @ RFM69 here!");
+  if (!bmp.begin(0x76))
+    isBMP280present = false; // BPM280 sensor was not found -- respond with empty data fields
+  else
+    isBMP280present = true;
   // <!-- RFM69 INIT
   bareRFM69::reset(RESET_PIN); // sent the RFM69 a hard-reset.
   rfm.setRecommended(); // set recommended paramters in RFM69.
   rfm.setPacketType(false, false); // set the used packet type.
   rfm.setBufferSize(2);   // set the internal buffer size.
-  rfm.setPacketLength(4); // set the packet length.
+  rfm.setPacketLength(64); // set the packet length.
   rfm.setFrequency((uint32_t) 915*1000*1000); // set the frequency.
   // baudrate is default, 4800 bps now.
   rfm.receive(); // set it to receiving mode.
-  /*
-      setup up interrupts such that we don't have to call poll() in a loop.
-  */
   rfm.setDioMapping1(RFM69_PACKET_DIO_2_AUTOMODE); // tell the RFM to represent whether we are in automode on DIO 2.
   pinMode(DIO2_PIN, INPUT); // set pinmode to input.
   SPI.usingInterrupt(INTERRUPT_NUMBER); // Tell the SPI library we're going to use the SPI bus from an interrupt.
@@ -68,35 +59,65 @@ void setup() {
   delay(5);
 }
 
-void loop() {
 
-  int dht = DHT11.read(DHTPIN);
-  float humi = DHT11.humidity;
-  float temp = DHT11.temperature;
 
-  switch (dht)
-  {
-    case DHTLIB_OK:
-    {
-		  Serial.println("OK");
-      Serial.print("Humidity (%): ");
-      Serial.println(humi, 2);
-      Serial.print("Temperature (°C): ");
-      Serial.println(temp, 2);
-      Serial.print("Heat index: ");
-      Serial.print(heatIndex(temp, humi));
-      Serial.println(" *C");
+// -----------------------------------------------------------------------------------------------------------
+String readBMP280()
+{ // C|мм.р.ст.|м
+ if (isBMP280present)
+ {
+  return "Temp2|" +String(bmp.readTemperature()) +"|Pres2|" +bmp.readPressure()/133.3 +"|Alt2|" +bmp.readAltitude(1013.25);
+ }
+ else
+  return "Temp2||Pres2||Alt2|";
+}
+
+
+
+// -----------------------------------------------------------------------------------------------------------
+void sender()
+{
+    uint32_t start_time = millis();
+    //String Buff;
+    //uint8_t length = 1;
+
+    while(true){
+        if (!rfm.canSend()){
+            continue; // sending is not possible, already sending.
+        }
+
+        if ((millis() - start_time) > 2000){ // every 500 ms.
+            start_time = millis();
+
+            // be a little bit verbose.
+            //Serial.print("Send Packet ("); Serial.print(length); Serial.print("): "); Serial.println(*counter);
+
+            // send the number of bytes equal to that set with setPacketLength.
+            // read those bytes from memory where counter starts.
+            String Buff = readBMP280();
+            uint8_t length = Buff.length() +1;
+            char buffer[length];
+            Buff.toCharArray(buffer,length);
+            rfm.send(&buffer);
+            Serial.println(Buff);
+            //rfm.sendVariable(&tx_buffer, length);
+
+            // length = (length + 1) % length_overflow;
+            // if (length == 0){
+            //     length++;
+            // }
+
+
+            //(*counter)++; // increase the counter.
+        }
+
     }
-		break;
-    case DHTLIB_ERROR_CHECKSUM:
-		  Serial.println("Checksum error");
-		break;
-    case DHTLIB_ERROR_TIMEOUT:
-		  Serial.println("Time out error");
-		break;
-    default:
-		  Serial.println("Unknown error");
-		break;
-  }
-  delay(2000);   // DHT11 is slow sensor, wait at least 2 sec
+}
+
+
+
+// -----------------------------------------------------------------------------------------------------------
+void loop()
+{
+  sender(); // -----
 }
