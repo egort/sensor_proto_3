6f2ec1da357364803c0334be511ebb699aebc9e1
egort <rogachevd@gmail.com>
2017-05-30 03:34:23 +0300

    add adafruit bmp280 lib & example

---
+++ lib/Adafruit_BMP280/bmp280test.ino
@@ -0,0 +1,37 @@
+/***************************************************************************
+http://ardu.net/ru/datchiki-i-sensory/363-modul-bmp280-i2c-datchik-davleniya-i-temperatury-module-pressure-novinka-spi-120768573.html
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+Adafruit_BMP280 bmp; // I2C
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BMP280 test"));
+  
+  if (!bmp.begin(0x76)) {  
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+}
+
+void loop() {
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+    
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure()/133.3);
+    Serial.println(" мм р.ст.");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); // this should be adjusted to your local forcase
+    Serial.println(" m");
+    
+    Serial.println();
+    delay(2000);
+}
